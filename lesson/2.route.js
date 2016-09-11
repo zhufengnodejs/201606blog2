3d37dbd8cbaeea8ac1ac7533b46aed9c09a14a78
zhufengnodejs <zhufengnodejs@126.com>
2016-09-11 11:23:35 +0800

    项目结构分析

--- lesson/2.route.js
+++ lesson/2.route.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var user = require('./routes/user');
 var app = express();
-
+app.get('/',function(req,res){
+    res.send('主页');
+});
 app.use('/user',user);
 app.listen(8080);
--- lesson/routes/user.js
+++ lesson/routes/user.js
@@ -3,4 +3,7 @@ var router = express.Router();
 router.get('/add',function(req,res){
     res.send('user add');
 });
+router.get('/delete',function(req,res){
+    res.send('user delete');
+});
 module.exports = router;
